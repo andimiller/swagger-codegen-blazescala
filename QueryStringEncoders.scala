a1f333418effff546cdfdc09b033f3434d583f5e
Andi Miller <andi@andimiller.net>
2016-09-30 13:20:08 +0100

    write my own QueryString typeclass in stead of using Show

---
+++ QueryStringEncoders.scala
@@ -0,0 +1,31 @@
+package querystringencoders
+
+object QueryStringEncoders {
+
+  trait QueryString[T] {
+    def queryString(t: T): String
+  }
+
+  implicit object QueryStringString extends QueryString[String] {
+    override def queryString(s: String): String = s
+  }
+
+  implicit object QueryStringLong extends QueryString[Long] {
+    override def queryString(t: Long): String = t.toString
+  }
+
+  implicit object QueryStringInteger extends QueryString[Integer] {
+    override def queryString(t: Integer): String = t.toString
+  }
+
+  implicit class QueryStringableObject[T](t: T) {
+    def queryString(implicit qsi: QueryString[T]): String = qsi.queryString(t)
+  }
+
+  implicit def listQueryString[T](implicit q: QueryString[T]): QueryString[List[T]] = new QueryString[List[T]] {
+    override def queryString(xs: List[T]): String = xs.foldLeft("") { (a, n) =>
+      if (a == "") n.queryString else a + "," + n.queryString
+    }
+  }
+
+}
