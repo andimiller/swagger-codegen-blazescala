08a1f7d0d1f8de00498b54b608e50b6116b5a20b
Andi Miller <andi@andimiller.net>
2017-08-29 20:26:19 +0000

    ditch all the classes for verbs, pass verb straight through

--- RequestTemplate.scala
+++ RequestTemplate.scala
@@ -8,6 +8,7 @@ trait RequestTemplate[I, O] {
   // Opaque for now. Might need Coproduct LiftAll
   def errorDecoder(status: Int): Decoder[Errors]
 
+  def body: I
   def maybeBody: Option[I]
   def queryParams: Map[String, String]
   def headerParams: Map[String, String]
@@ -20,26 +21,4 @@ object RequestTemplate {
   type Aux[I, O, E] = RequestTemplate[I, O] {
     type Errors = E
   }
-}
-
-trait GETRequestTemplate[I, O] extends RequestTemplate[Unit, O] {
-  override final val maybeBody = None
-  val httpMethod = Method.GET
-}
-
-trait PUTRequestTemplate[I, O] extends RequestTemplate[I, O] {
-  override final def maybeBody = Some(body)
-  def body: I
-  val httpMethod = Method.PUT
-}
-
-trait POSTRequestTemplate[I, O] extends RequestTemplate[I, O] {
-  override final def maybeBody = Some(body)
-  def body: I
-  val httpMethod = Method.POST
-}
-
-trait DELETERequestTemplate[I, O] extends RequestTemplate[Unit, Unit] {
-  override final val maybeBody = None
-  val httpMethod = Method.DELETE
 }
